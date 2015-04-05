4aef16021e5004a0e1478e011c999bae267b6f2e
Wen Qin <Elfenwen@Wens-MacBook-Pro.local>
2015-04-05 16:05:50 -0700

    add trial.js

---
+++ trial.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 56789;
+var router = express.router();
+
+router.router('/home').get(function(req, res){
+	console.log('You are at home...');
+	res.send('Welcome home...');
+});
+
+router.route('/home/:param').get(function(req, res) {
+	var who = req.params.param;
+	console.log(who + ' is at home...');
+	res.send({ 
+		who : who,
+		where : "home"
+	});
+});
+
+app.use('/', router);
+app.listen(port, function(){
+	console.log('Trial server has started at: ' + port);
+})

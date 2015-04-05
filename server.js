1c036272c1f8af24bc34358dd871eaf971e46a5b
Wen Qin <Elfenwen@Wens-MacBook-Pro.local>
2015-04-05 16:04:59 -0700

    add trial.js

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 8080
-app.use('/', function(req, res){
-    console.log('entered');
-    res.send('hello world');
-});
+var port = process.env.PORT || 12345
 
+var router = express.Router();
+
+router.route('/')
+	.get(function(req, res, next) {
+	    res.send('hello world');
+	});
+
+router.route("/:str")
+	.get(function(req, res) {
+		console.log("in " + req.params.str);
+		res.send({ hello : "wenqin" });
+	});
+
+app.use('/', router);
 app.listen(port, function(){
     console.log('server started at port: ' + port);
 });
